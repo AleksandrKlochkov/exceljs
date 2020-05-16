51f9cf0270e4a98f8ab8e4a59f0e969f9416e6fc
unknown <surfer.ru@mail.ru>
2020-05-17 00:00:43 +0300

    create functionals resize table

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,9 +1,49 @@
 import {ExcelComponent} from '@core/ExcelComponent'
-import { createTable } from './table.template'
+import {createTable} from './table.template'
+import {$} from '@core/dom'
+
 
 export class Table extends ExcelComponent {
     static className = 'excel__table'
+
+    constructor($root) {
+        super($root, {
+            listeners: ['mousedown']
+        //   listeners: ['click', 'mousedown', 'mousemove', 'mouseup']
+        })
+    }
+
     toHTML() {
         return createTable(20)
     }
+
+    onMousedown(event) {
+       if (event.target.dataset.resize) {
+            const $resizer = $(event.target)
+            const $parent = $resizer.closest('[data-type="resizable"]')
+            const coords = $parent.getCoords()
+            console.log($parent.getCoords())
+            document.onmousemove = e => {
+                const delta = e.pageX - coords.right
+                const value = coords.width + delta
+                $parent.$el.style.width = value + 'px'
+            }
+
+            document.onmouseup = () => {
+                document.onmousemove = null
+            }
+       }
+    }
+
+    // onClick(event) {
+    //     console.log('click', event.target)
+    // }
+
+    // onMousemove() {
+    //     console.log('mousemove')
+    // }
+
+    // onMouseup() {
+    //     console.log('mouseup')
+    // }
 }
--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -6,20 +6,30 @@ const CODES = {
 function toCell() {
     // selected
     return `
-        <div class="cell " contenteditable="">A1</div>
+        <div class="cell " contenteditable=""></div>
     `
 }
 
 function toColumn(col) {
     return `
-        <div class="column">${col}</div>
+        <div class="column"data-type="resizable">
+            ${col}
+            <div class="col-resize" data-resize="col"></div>
+        </div>
     `
 }
 
 function createRow(index, content) {
+    const resize = index ?
+    '<div class="row-resize" data-resize="row"></div>' :
+    ''
+
     return `
         <div class="row">
-            <div class="row-info">${index !==null ? +index : ''}</div>
+            <div class="row-info">
+                ${index !==null ? +index : ''}
+                ${resize}
+            </div>
             <div class="row-data">${content}</div>
         </div>
     `
