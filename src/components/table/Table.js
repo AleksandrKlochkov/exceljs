d322422d3e8d0b304362eb224c4546b73ed2ba9f
unknown <surfer.ru@mail.ru>
2020-05-17 13:04:32 +0300

    add functionals resize table

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,6 +1,7 @@
 import {ExcelComponent} from '@core/ExcelComponent'
 import {createTable} from './table.template'
-import {$} from '@core/dom'
+import { resizeHandler } from './table.resize'
+import { shouldResize } from './table.fucntions'
 
 
 export class Table extends ExcelComponent {
@@ -18,20 +19,8 @@ export class Table extends ExcelComponent {
     }
 
     onMousedown(event) {
-       if (event.target.dataset.resize) {
-            const $resizer = $(event.target)
-            const $parent = $resizer.closest('[data-type="resizable"]')
-            const coords = $parent.getCoords()
-            console.log($parent.getCoords())
-            document.onmousemove = e => {
-                const delta = e.pageX - coords.right
-                const value = coords.width + delta
-                $parent.$el.style.width = value + 'px'
-            }
-
-            document.onmouseup = () => {
-                document.onmousemove = null
-            }
+       if (shouldResize(event)) {
+        resizeHandler(event, this.$root)
        }
     }
 
